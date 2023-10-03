4af8623cc80268b4ee7f21add429f577a6e9dfa5
Min-Ki Park <contact@minki.pk>
2023-10-03 22:04:10 +0900

    fix: inline line break

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,11 +1,13 @@
-import { declareIndexPlugin, PowerupNamespace, ReactRNPlugin } from "@remnote/plugin-sdk";
+import { declareIndexPlugin, ReactRNPlugin } from "@remnote/plugin-sdk";
 
 export const [CUSTOM_ICON, IconTag] = ["custom-icon_powerup", "75px"];
 
+export const sizeFixer = "Custom Icon";
+
 const sizeFix = `
-  [data-rem-container-tags~="75px"] {
-    overflow-x: auto;
-    overflow-y: hidden;
+  [data-rem-container-tags~="75px"],
+  [data-document-tags~="75px"] [data-rem-container-tags*="table"] {
+    overflow: hidden;
   }
   [data-rem-container-tags~="75px"] .inline-block {
     width: fit-content;
@@ -20,31 +22,59 @@ const sizeFix = `
   }
 `;
 
+const New = `[data-document-tags*="table"] div.group.inline-block.align-text-top {
+  max-width: 3rem !important;
+  max-height: 3rem !important;
+}`;
+
 async function onActivate(plugin: ReactRNPlugin) {
-  await plugin.app.registerPowerup("Custom Icon", CUSTOM_ICON, "A Power-up Rem for saving rem icons", { slots: [] });
+  await plugin.app.registerPowerup("Custom Icon", CUSTOM_ICON, "A Power-up Rem for saving rem icons", {
+    slots: [
+      {
+        name: "sizeFixer",
+        code: "size-fixer",
+        hidden: true,
+        onlyProgrammaticModifying: false,
+      },
+    ],
+  });
 
   // async function add(IconTag: string) {
   //   const docTitle = await plugin.powerup.getPowerupByCode(CUSTOM_ICON);
-  //   await docTitle?.addTag(IconTag); 
+  //   await docTitle?.addTag(IconTag);
   // }
-  
-  const remIcon = await plugin.powerup.getPowerupByCode(CUSTOM_ICON);
-  await remIcon?.addTag(IconTag);
-  
-  const iconTitle = await remIcon?.text[0];
 
-  if (iconTitle == "Custom Icon") {
-    await plugin.app.registerCSS("sizeFix", sizeFix);
-    const title = await plugin.window.getOpenPaneRemIds();
-  } else {
-    return;
-  }
+  // if (iconTitle == "Custom Icon") {
+  //   await plugin.app.registerCSS("sizeFix", sizeFix);
+  //   const title = await plugin.window.getOpenPaneRemIds();
+  // } else {
+  //   return;
+  // }
+  // const AA = await plugin.rem.findOne("dCweTM5r4UecrpV8S");
+
+  // console.log(AA);
+
+  // await AA?.addTag("75px");
 
   await plugin.app.registerCommand({
     id: "rem-icon",
     name: "Custom Icon",
+    quickCode: "icon",
+    keyboardShortcut: "opt+shift+i",
     action: async () => {
+      const remIcon = await plugin.powerup.getPowerupByCode(CUSTOM_ICON);
+      await remIcon?.addTag(IconTag);
       await remIcon?.openRemAsPage();
+
+      await plugin.app.registerCSS("New", New);
+      try {
+        // await plugin.app.registerCSS("")
+      } catch (error) {
+        console.log("Minimal Table is not installed");
+      }
+      // const iconTitle = await remIcon?.text[0];
+      console.log(CUSTOM_ICON);
+      console.log(IconTag);
     },
   });
 }
